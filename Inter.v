a7c887bd24c387df068d4281a9aee6ef584e0529
brunosoliveira <devnull@localhost>
2014-06-09 02:55:37 -0400

    Different approach to model subtyping: using an inductive relation instead of function.
    Reflexivity is now trivial.

--- Inter.v
+++ Inter.v
@@ -29,40 +29,63 @@ subSigma2 (Typ t1)    t2 i = subTyp t1 t2 i
 *)
 
 Require Import Arith.
+Require Import Arith.MinMax.
 
 Check eq_nat_dec.
 
-Fixpoint subTyp (t1 : PTyp nat) (t2 : PTyp nat) (i : nat) : Prop := 
-  match (t1, t2) with 
-      | (PInt,PInt) => True
-      | (Var x, Var y) => if (eq_nat_dec x y) then True else False
-      | (Forall f, Forall g) => subTyp (f i) (g i) (i+1)
-      | (And o1 o2, And o3 o4) => and (subTyp o1 o3 i) (subTyp o2 o4 i)
-      (*| (Fun o1 o2, Fun o3 o4) => and (subTyp o3 o1 i) (subTyp o2 o4 i) *)
-      | (_,_) => False
+Check max.
+
+Fixpoint size (t : PTyp nat) (i : nat) : nat :=
+  match t with
+      | PInt => 1
+      | Var x => 1
+      | Forall f => 1 + size (f i) (i+1)
+      | And o1 o2 => 1 + max (size o1 i) (size o2 i)
+      | Fun o1 o2 => 1 + max (size o1 i) (size o2 i)
   end.
 
-Lemma reflex : forall (t1 : PTyp nat) (n : nat), subTyp t1 t1 n.
+(* Subtyping relation *)
+
+Inductive sub : nat -> PTyp nat -> PTyp nat -> Prop :=
+  | SInt : forall i, sub i (PInt nat) (PInt nat)
+  | SVar : forall i x y, x = y -> sub i (Var nat x) (Var nat y)
+  | SForall : forall i f g, sub (i+1) (f i) (g i) -> sub i (Forall nat f) (Forall nat g)
+  | SAnd : forall i o1 o2 o3 o4, sub i o1 o3 -> sub i o2 o4 -> sub i (And nat o1 o2) (And nat o3 o4)
+  | SFun : forall i o1 o2 o3 o4, sub i o3 o1 -> sub i o2 o4 -> sub i (Fun nat o1 o2) (Fun nat o3 o4).
+
+Lemma reflex : forall t1 i, sub i t1 t1.
 Proof.
-intro.
-induction t1; intro.
-(* case Var *)
-simpl.
-destruct (eq_nat_dec a a).
-auto.
-destruct n0.
+induction t1; intros.
+(* Case Var *)
+apply SVar.
 reflexivity.
-(* case PInt *)
-simpl.
-reflexivity.
+(* Case SInt *)
+apply SInt.
 (* case Forall *)
-simpl.
+apply SForall.
 apply H.
-(* Case Fun *)
-admit.
-(* Case And *)
-simpl.
-split.
+(* case Fun *)
+apply SFun.
 apply IHt1_1.
 apply IHt1_2.
-Qed.
+(* case And *)
+apply SAnd.
+apply IHt1_1.
+apply IHt1_2.
+Defined.
+
+(* A functional definition : algorithm *)
+
+Fixpoint subTyp (n : nat) (t1 : PTyp nat) (t2 : PTyp nat) (i : nat) : Prop  :=
+  match n with 
+      | 0 => False
+      | S m =>
+          match (t1,t2) with 
+            | (PInt,PInt) => True
+            | (Var x, Var y) => if (eq_nat_dec x y) then True else False
+            | (Forall f, Forall g) => subTyp m (f i) (g i) (i+1)
+            | (And o1 o2, And o3 o4) => and (subTyp m o1 o3 i) (subTyp m o2 o4 i)
+            | (Fun o1 o2, Fun o3 o4) => and (subTyp m o3 o1 i) (subTyp m o2 o4 i)
+            | (_,_) => False
+          end
+  end.
