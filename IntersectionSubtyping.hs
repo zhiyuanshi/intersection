8ccf03f305455e0269935df60f793a0d1b076c09
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2014-06-19 22:44:12 +0800

    .lhs -> .hs

--- IntersectionSubtyping.hs
+++ IntersectionSubtyping.hs
@@ -1,46 +1,46 @@
-> module IntersectionSubtyping where
+module IntersectionSubtyping where
 
-Syntax:
+-- Syntax:
 
-t ::= a | forall a . o | o1 -> o2 | Int
-o ::= o1 & o2 | t 
+-- t ::= a | forall a . o | o1 -> o2 | Int
+-- o ::= o1 & o2 | t 
 
-> data PTyp a = Var a | Forall (a -> PSigma a) | Fun (PSigma a) (PSigma a) | PInt 
->
-> data PSigma a = And (PSigma a) (PSigma a) | Typ (PTyp a)
+data PTyp a = Var a | Forall (a -> PSigma a) | Fun (PSigma a) (PSigma a) | PInt 
 
-e ::= x | \(x : o) . e | e1 e2 | /\a . e | e t | e1 & e2
+data PSigma a = And (PSigma a) (PSigma a) | Typ (PTyp a)
 
-> data PExp t e = 
->     EVar e 
->   | ELam (PSigma t) (e -> PExp t e) 
->   | EApp (PExp t e) (PExp t e) 
->   | ETLam (t -> PExp t e)
->   | ETApp (PExp t e) (PSigma t)
->   | EAnd (PExp t e) (PExp t e)  
+-- e ::= x | \(x : o) . e | e1 e2 | /\a . e | e t | e1 & e2
 
-> -- Int & Int
-> t1 = And (Typ PInt) (Typ PInt)
+data PExp t e = 
+    EVar e 
+  | ELam (PSigma t) (e -> PExp t e) 
+  | EApp (PExp t e) (PExp t e) 
+  | ETLam (t -> PExp t e)
+  | ETApp (PExp t e) (PSigma t)
+  | EAnd (PExp t e) (PExp t e)  
 
-> -- Int
-> t2 = Typ PInt
+-- Int & Int
+t1 = And (Typ PInt) (Typ PInt)
 
-> -- Int & (Int -> Int)
-> t3 = And (Typ PInt) t4
+-- Int
+t2 = Typ PInt
 
-> -- (Int -> Int) & Int
-> t7 = And t4 (Typ PInt)
+-- Int & (Int -> Int)
+t3 = And (Typ PInt) t4
 
-> -- Int -> Int
-> t4 = Typ (Fun (Typ PInt) (Typ PInt))
+-- (Int -> Int) & Int
+t7 = And t4 (Typ PInt)
 
-> -- forall a . a -> a 
-> t5 = Typ (Forall (\a -> Typ (Fun (Typ (Var a)) (Typ (Var a)))))
+-- Int -> Int
+t4 = Typ (Fun (Typ PInt) (Typ PInt))
 
-> -- forall a . Int & a -> a
-> t6 = Typ (Forall (\a -> Typ (Fun (And (Typ PInt) (Typ (Var a))) (Typ (Var a)))))
+-- forall a . a -> a 
+t5 = Typ (Forall (\a -> Typ (Fun (Typ (Var a)) (Typ (Var a)))))
 
-Subtyping Relation:
+-- forall a . Int & a -> a
+t6 = Typ (Forall (\a -> Typ (Fun (And (Typ PInt) (Typ (Var a))) (Typ (Var a)))))
+
+{- Subtyping Relation:
 
 ----------
 |t1 <: t2|
@@ -83,47 +83,48 @@ o1 & o2 <: t
 t1 <: t2
 --------                           Sub-tt
 t1 <: t2
+-}
 
-> subTyp :: PTyp Int -> PTyp Int -> Int -> Bool
-> subTyp PInt    PInt             _ = True
-> subTyp (Var x) (Var y)          _ = x == y
-> subTyp (Forall f) (Forall g)    i = subSigma (f i) (g i) (i+1)
-> subTyp (Fun o1 o2) (Fun o3 o4)  i = subSigma o3 o1 i && subSigma o2 o4 i 
-> subTyp _           _            _ = False
->
-> subSigma :: PSigma Int -> PSigma Int -> Int -> Bool
-> subSigma o (And o1 o2) i = subSigma o o1 i && subSigma o o2 i 
-> subSigma o (Typ t)     i = subSigma2 o t i
+subTyp :: PTyp Int -> PTyp Int -> Int -> Bool
+subTyp PInt    PInt             _ = True
+subTyp (Var x) (Var y)          _ = x == y
+subTyp (Forall f) (Forall g)    i = subSigma (f i) (g i) (i+1)
+subTyp (Fun o1 o2) (Fun o3 o4)  i = subSigma o3 o1 i && subSigma o2 o4 i 
+subTyp _           _            _ = False
 
-> subSigma2 :: PSigma Int -> PTyp Int -> Int -> Bool
-> subSigma2 (And o1 o2) t         i  = subSigma2 o1 t i || subSigma2 o2 t i
-> subSigma2 (Typ t1)    t2        i  = subTyp t1 t2 i
+subSigma :: PSigma Int -> PSigma Int -> Int -> Bool
+subSigma o (And o1 o2) i = subSigma o o1 i && subSigma o o2 i 
+subSigma o (Typ t)     i = subSigma2 o t i
 
-> sub o1 o2 = subSigma o1 o2 0
+subSigma2 :: PSigma Int -> PTyp Int -> Int -> Bool
+subSigma2 (And o1 o2) t  i = subSigma2 o1 t i || subSigma2 o2 t i
+subSigma2 (Typ t1)    t2 i = subTyp t1 t2 i
 
-> tcheck :: PExp Int (PSigma Int) -> Int -> Maybe (PSigma Int)
-> tcheck (EVar t)    _  = Just t 
-> tcheck (ELam s f)  i  = 
->   do tbody <- tcheck (f s) i
->      return (Typ (Fun s tbody))
-> tcheck (EApp e1 e2) i = 
->   do t1 <- tcheck e1 i
->      t2 <- tcheck e2 i
->      case t1 of 
->         Typ (Fun t3 t4) | subSigma t2 t3 i  -> Just t4 
->         _                                   -> Nothing
-> tcheck (ETLam f) i =   
->   do t <- tcheck (f i) (i+1)
->      return (Typ (Forall (\x -> subst i (Typ (Var x)) t))) 
-> tcheck (ETApp e o) i = 
->   do t <- tcheck e i
->      case t of
->         Typ (Forall f) -> Just (subst i o (f i)) 
->         otherwise -> Nothing
-> tcheck (EAnd e1 e2) i = 
->   do t1 <- tcheck e1 i
->      t2 <- tcheck e2 i
->      return (And t1 t2)
->
-> subst :: Int -> PSigma Int -> PSigma Int -> PSigma Int
-> subst = error "TODO!"
+sub o1 o2 = subSigma o1 o2 0
+
+tcheck :: PExp Int (PSigma Int) -> Int -> Maybe (PSigma Int)
+tcheck (EVar t)    _  = Just t 
+tcheck (ELam s f)  i  = 
+  do tbody <- tcheck (f s) i
+     return (Typ (Fun s tbody))
+tcheck (EApp e1 e2) i = 
+  do t1 <- tcheck e1 i
+     t2 <- tcheck e2 i
+     case t1 of 
+        Typ (Fun t3 t4) | subSigma t2 t3 i  -> Just t4 
+        _                                   -> Nothing
+tcheck (ETLam f) i =   
+  do t <- tcheck (f i) (i+1)
+     return (Typ (Forall (\x -> subst i (Typ (Var x)) t))) 
+tcheck (ETApp e o) i = 
+  do t <- tcheck e i
+     case t of
+        Typ (Forall f) -> Just (subst i o (f i)) 
+        otherwise -> Nothing
+tcheck (EAnd e1 e2) i = 
+  do t1 <- tcheck e1 i
+     t2 <- tcheck e2 i
+     return (And t1 t2)
+
+subst :: Int -> PSigma Int -> PSigma Int -> PSigma Int
+subst = error "TODO!"
