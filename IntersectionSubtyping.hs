f87274e3a7cf70530840d13c31432b01f6af9e63
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2014-06-19 22:55:14 +0800

    add rough implementation of printy printing

--- IntersectionSubtyping.hs
+++ IntersectionSubtyping.hs
@@ -1,4 +1,8 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+{-# LANGUAGE FlexibleInstances, RecordWildCards #-}
 module IntersectionSubtyping where
+
+import Text.PrettyPrint
 
 -- Syntax:
 
@@ -18,6 +22,51 @@ data PExp t e =
   | ETLam (t -> PExp t e)
   | ETApp (PExp t e) (PSigma t)
   | EAnd (PExp t e) (PExp t e)  
+
+precApp :: Int
+precApp = 1
+
+precFun :: Int
+precFun = 2
+
+parenPrec :: Int -> Int -> Doc -> Doc
+parenPrec inheritedPrec currentPrec t
+    | inheritedPrec <= 0          = t
+    | inheritedPrec < currentPrec = parens t
+    | otherwise                   = t
+
+data L = L
+    { lx :: Int  -- fresh label for variables
+    , la :: Int  -- fresh label for type variables
+    } deriving (Eq, Show)
+
+class Pretty a where
+    pretty :: a -> Doc
+    pretty = prettyPrec 0 L{ lx = 1, la = 1 }
+  
+    prettyPrec :: Int -> L -> a -> Doc
+    prettyPrec _ _ = pretty
+
+instance Pretty (PTyp Int) where
+    prettyPrec p L{..} (Var a)     = text ("A" ++ show a)
+    prettyPrec p L{..} (Forall f)  = text ("forall A" ++ show la ++ ".") <+> prettyPrec p L{ la = la + 1, .. } (f la)
+    prettyPrec p L{..} (Fun t1 t2) = parenPrec p precFun $ prettyPrec (precFun - 1) L{..} t1 <+> text "->" <+> prettyPrec p L{..} t2
+    prettyPrec p L{..} PInt        = text "Int"
+
+instance Pretty (PSigma Int) where
+    prettyPrec p L{..} (And s1 s2) = parens (prettyPrec p L{..} s1 <+> text "/\\" <+> prettyPrec p L{..} s2)
+    prettyPrec p L{..} (Typ t)     = prettyPrec p L{..} t
+
+instance Pretty (PExp Int Int) where
+    prettyPrec p L{..} (EVar x)     = text ("x" ++ show x)
+    prettyPrec p L{..} (ELam t f)   = char '\\' <> 
+                                                parens (text ("x" ++ show lx) <+> colon <+> prettyPrec p L{ lx = lx + 1, .. } t) <> 
+                                                char '.' <+> 
+                                                prettyPrec p L{ lx = lx + 1, .. } (f lx)
+    prettyPrec p L{..} (ETLam f)    = text "/\\" <> text ("A" ++ show la) <> char '.' <+> prettyPrec p L{ la = la + 1, .. } (f la)
+    prettyPrec p L{..} (ETApp e t)  = prettyPrec p L{..} e <+> prettyPrec p L{..} t
+    prettyPrec p L{..} (EApp e1 e2) = prettyPrec precApp L{..} e1 <+> prettyPrec precApp L{..} e2 
+    prettyPrec p L{..} (EAnd e1 e2) = prettyPrec precApp L{..} e1 <+> text "/\\" <+> prettyPrec precApp L{..} e2 
 
 -- Int & Int
 t1 = And (Typ PInt) (Typ PInt)
