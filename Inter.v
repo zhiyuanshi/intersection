2c67061819915b5fc185c5db0c4b7670874964d3
brunosoliveira <devnull@localhost>
2014-08-03 23:15:06 +0800

    Trying a different approach to prove transitivity.

--- Inter.v
+++ Inter.v
@@ -41,38 +41,8 @@ t1 & t2 <: t
 
 *)
 
-(*
-subTyp :: PTyp Int -> PTyp Int -> Int -> Bool
-subTyp PInt    PInt             _ = True
-subTyp (Var x) (Var y)          _ = x == y
-subTyp (Forall f) (Forall g)    i = subSigma (f i) (g i) (i+1)
-subTyp (Fun o1 o2) (Fun o3 o4)  i = subSigma o3 o1 i && subSigma o2 o4 i
-subTyp _           _            _ = False
-
-subSigma :: PSigma Int -> PSigma Int -> Int -> Bool
-subSigma o (And o1 o2) i = subSigma o o1 i && subSigma o o2 i
-subSigma o (Typ t)     i = subSigma2 o t i
-
-subSigma2 :: PSigma Int -> PTyp Int -> Int -> Bool
-subSigma2 (And o1 o2) t  i = subSigma2 o1 t i || subSigma2 o2 t i
-subSigma2 (Typ t1)    t2 i = subTyp t1 t2 i
-*)
-
 Require Import Arith.
 Require Import Setoid.
-
-Check eq_nat_dec.
-
-Check max.
-
-Fixpoint size (t : PTyp nat) (i : nat) : nat :=
-  match t with
-      | PInt => 1
-      | Var x => 1
-      | Forall f => 1 + size (f i) (i+1)
-      | And o1 o2 => 1 + max (size o1 i) (size o2 i)
-      | Fun o1 o2 => 1 + max (size o1 i) (size o2 i)
-  end.
 
 (* Subtyping relation *)
 
@@ -86,35 +56,30 @@ Inductive sub : nat -> PTyp nat -> PTyp nat -> Prop :=
   | SAnd2 : forall i t t1 t2, sub i t1 t -> sub i (And nat t1 t2) t
   | SAnd3 : forall i t t1 t2, sub i t2 t -> sub i (And nat t1 t2) t.
 
-Definition equiv i t1 t2 := sub i t1 t2 /\ sub i t2 t1.
-
-Definition contextEq i t1 t2 := (forall t, sub i t1 t -> sub i t2 t) /\ (forall t, sub i t t1 -> sub i t t2).
+Hint Resolve SVar SInt SForall SFun SAnd1 SAnd2 SAnd3.
 
 Lemma reflex : forall t1 i, sub i t1 t1.
 Proof.
-induction t1; intros.
-(* Case Var *)
-apply SVar.
-(* Case SInt *)
-apply SInt.
-(* case Forall *)
-apply SForall.
-apply H.
-(* case Fun *)
-apply SFun.
-apply IHt1_1.
-apply IHt1_2.
-(* case And *)
-apply SAnd1.
-apply SAnd2.
-apply IHt1_1.
-apply SAnd3.
-apply IHt1_2.
+induction t1; intros; auto.
 Defined.
 
+Definition narrowing_sub P Q X S T := forall i, sub i P Q -> (sub i X Q -> sub i S T) -> sub i X P -> sub i S T.
+
+Definition transitivity_sub S Q T := forall i, sub i S Q -> sub i Q T -> sub i S T.
+
+Lemma narrowing : forall X P Q S T, transitivity_sub X P Q -> narrowing_sub P Q X S T.
+unfold narrowing_sub; intros.
+apply H1.
+apply H.
+exact H2.
+exact H0.
+Defined.
 
 Lemma invAndS1 : forall t t1 t2 i, sub i t (And nat t1 t2) -> sub i t t1 /\ sub i t t2. 
 Proof.
+(*
+induction t; intros; split; try (inversion H); auto.
+*)
 induction t; intros.
 (* Case Var *)
 inversion H.
@@ -207,6 +172,102 @@ exact H3.
 admit.
 admit.
 Defined.
+
+Lemma trans : forall Q S T, transitivity_sub S Q T.
+induction Q.
+unfold transitivity_sub; intros.
+induction T; try (inversion H0); auto. 
+rewrite H4 in H. auto.
+unfold transitivity_sub; intros.
+induction T; try (inversion H0); auto.
+(* Case Forall *)
+unfold transitivity_sub; intros.
+induction T; try (inversion H1); auto.
+induction S; try (inversion H0); auto.
+apply SForall.
+unfold transitivity_sub in H.
+apply (H i); auto.
+admit.
+admit.
+(* Case Fun *)
+unfold transitivity_sub; intros.
+generalize H0 H. clear H0. clear H.
+generalize S. clear S.
+induction T; induction S; intros. 
+inversion H0.
+inversion H0.
+inversion H0.
+inversion H0.
+inversion H0.
+inversion H0.
+inversion H0.
+inversion H0.
+inversion H0.
+inversion H0.
+inversion H0.
+inversion H0.
+inversion H1.
+inversion H0.
+inversion H0.
+inversion H.
+inversion H.
+inversion H1.
+(* Case 1 *)
+inversion H0.
+inversion H.
+apply SFun.
+apply IHQ1; auto.
+apply IHQ2; auto.
+inversion H.
+(* Case 2 *)
+apply SAnd2.
+apply IHS1.
+exact H0.
+exact H5.
+apply SAnd3.
+apply IHS2; auto.
+inversion H.
+inversion H.
+inversion H1.
+(* Case 3 *)
+assert (sub i (Fun nat Q1 Q2) T1 /\ sub i (Fun nat Q1 Q2) T2).
+apply invAndS1; auto.
+destruct H1.
+apply SAnd1.
+apply IHT1; auto.
+apply IHT2; auto.
+assert (sub i (Fun nat Q1 Q2) T1 /\ sub i (Fun nat Q1 Q2) T2).
+apply invAndS1; auto.
+destruct H1.
+assert (sub i S1 (Fun nat Q1 Q2) \/ sub i S2 (Fun nat Q1 Q2)).
+apply invAndS2; auto.
+destruct H3.
+apply SAnd2.
+apply IHS1.
+exact H0.
+exact H3.
+apply SAnd3.
+apply IHS2.
+auto.
+auto.
+(* Case And *)
+unfold transitivity_sub; intros.
+assert (sub i S Q1 /\ sub i S Q2).
+apply invAndS1; auto.
+destruct H1.
+assert (sub i Q1 T \/ sub i Q2 T).
+apply invAndS2; auto.
+destruct H3.
+apply IHQ1; auto.
+apply IHQ2; auto.
+Defined.
+
+Definition equiv i t1 t2 := sub i t1 t2 /\ sub i t2 t1.
+
+Definition contextEq i t1 t2 := (forall t, sub i t1 t -> sub i t2 t) /\ (forall t, sub i t t1 -> sub i t t2).
+
+
+
 
 Definition substitutability : 
   forall t1 t2 i, equiv i t2 t1 -> contextEq i t2 t1 /\ contextEq i t1 t2.
@@ -399,7 +460,14 @@ inversion H1.
 exact H5.
 Defined.
 
-Lemma FunInv : forall t t1 t2 t3 t4 i, (forall t : PTyp nat, sub i (Fun nat t1 t2) t -> sub i (Fun nat t3 t4) t) -> 
+Lemma FunInv1 : forall o t t1 t2 t3 t4 i, (forall t : PTyp nat, sub i (Fun nat t1 t2) t -> sub i (Fun nat t3 t4) t) -> 
+               sub i t1 t -> sub i o t.
+intros.
+(* assert (sub i (Fun nat t3 t4) (Fun )). *) 
+admit.
+Defined.
+
+Lemma FunInv2 : forall t t1 t2 t3 t4 i, (forall t : PTyp nat, sub i (Fun nat t1 t2) t -> sub i (Fun nat t3 t4) t) -> 
                sub i t2 t -> sub i t4 t.
 intros.
 assert (exists t10, sub i (Fun nat t3 t4) (Fun nat t10 t)).
@@ -446,16 +514,40 @@ apply SAnd3.
 exact H7.
 exact H6.
 (* Case Fun *)
+(*
+generalize i, o1, o2, o3, o4 , s1, s2, IHs1, IHs2, H.
+clear H. clear IHs2. clear IHs1. clear s2. clear s1. clear o4. clear o3. clear o2. clear o1. clear i.
+induction t2; intros.
+assert (sub i (Fun nat o3 o4) (Var nat a)).
+apply H. apply reflex.
+inversion H0.
+admit.
+admit.
+apply SFun.
+*)
+
 assert (sub i (Fun nat o3 o4) t2).
 apply H.
 apply reflex.
 induction t2; try (inversion H0).
 apply SFun.
-admit. 
+assert (sub i o3 t2_1).
+apply IHs1.
+intros.
+assert (sub i (Fun nat o3 o4) (Fun nat (Fun nat o1 o2) t2_1)). 
+apply H.
+apply SFun.
+apply IHt2_1.
+intros.
+apply H.
 
+(* Using H! *) 
+apply (FunInv1 o1 _ t2_1 t2_2 o3 o4). 
+apply H.
+admit.
 apply IHs2.
 intro.
-apply (FunInv _ t2_1 _ o3).
+apply (FunInv2 _ t2_1 _ o3).
 apply H.
 apply SAnd1.
 apply IHt2_1.
@@ -543,6 +635,15 @@ exact H.
 Defined.
 
 (* A functional definition : algorithm *)
+
+Fixpoint size (t : PTyp nat) (i : nat) : nat :=
+  match t with
+      | PInt => 1
+      | Var x => 1
+      | Forall f => 1 + size (f i) (i+1)
+      | And o1 o2 => 1 + max (size o1 i) (size o2 i)
+      | Fun o1 o2 => 1 + max (size o1 i) (size o2 i)
+  end.
 
 Fixpoint subTyp (n : nat) (t1 : PTyp nat) (t2 : PTyp nat) (i : nat) : Prop  :=
   match n with
