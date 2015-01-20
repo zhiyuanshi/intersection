00f3a1add74f5fda4977a70873e339e2893a5c3c
Zhiyuan Shi <zhiyuan.shi@gmail.com>
2015-01-20 12:39:34 +0800

    Follow Ruby conventions

--- paper/compute_positions.rb
+++ paper/compute_positions.rb
@@ -2,32 +2,32 @@
 require 'fileutils'
 
 # Example 1 - Read File and close
-def scan(fileName, beginpos, endpos, fileloc)
-  file = File.new(fileName, "r")
-  counter = 1
-  while (line = file.gets)
-    if line =~ /BEGIN_(\w+)/
-      throw "Symbol '#{$1}' already defined" if fileloc[$1]
-      fileloc[$1] = fileName
-      beginpos[$1] = counter+1
-      puts "BEGIN #{$1}: #{beginpos[$1]}"
+def scan(filename, beginpos, endpos, fileloc)
+  File.open(filename, "r") do |file|
+    counter = 1
+    while line = file.gets
+      if line =~ /BEGIN_(\w+)/
+        raise "Symbol '#{$1}' already defined" if fileloc[$1]
+        fileloc[$1] = filename
+        beginpos[$1] = counter + 1
+        puts "BEGIN #{$1}: #{beginpos[$1]}"
+      end
+      if line =~ /END_(\w+)/
+        endpos[$1] = counter - 1
+        puts "END #{$1}: #{endpos[$1]}"
+      end
+      counter += 1
     end
-    if line =~ /END_(\w+)/
-      endpos[$1] = counter-1
-      puts "END #{$1}: #{endpos[$1]}"
-    end
-    counter = counter + 1
   end
-  file.close
 end
 
 def process(file, out, beginpos, endpos, fileloc)
-  while (line = file.gets)
+  while line = file.gets
     if line =~ /APPLY:(\w+)=(\w+)/
       var = $1
       label = $2
-      line = line.sub( /#{var}=([0-9-]*)/, "#{var}=#{beginpos[label]}-#{endpos[label]}" )
-      line = line.sub( /\{[^{]*\}/, "{#{fileloc[label]}}" )
+      line = line.sub(/#{var}=([0-9-]*)/, "#{var}=#{beginpos[label]}-#{endpos[label]}")
+      line = line.sub(/\{[^{]*\}/, "{#{fileloc[label]}}")
     end
     out.write(line)
   end
@@ -39,24 +39,24 @@ fileloc = {}
 beginpos = {}
 endpos = {}
 
-Dir['../src/*.sf'].each do |file| 
+Dir["../src/*.sf"].each do |file|
   scan(file, beginpos, endpos, fileloc)
 end
-#Dir['../srcscala/src/*/*.scala'].each do |file| 
+#Dir['../srcscala/src/*/*.scala'].each do |file|
 #  scan(file, beginpos, endpos, fileloc)
 #end
-#Dir['../src/*.java'].each do |file| 
+#Dir['../src/*.java'].each do |file|
 #  scan(file, beginpos, endpos, fileloc)
 #end
-#Dir['../src/*/*.java'].each do |file| 
+#Dir['../src/*/*.java'].each do |file|
 #  scan(file, beginpos, endpos, fileloc)
 #end
-#Dir['../code/*/*/*/*/*.java'].each do |file| 
+#Dir['../code/*/*/*/*/*.java'].each do |file|
 #  scan(file, beginpos, endpos, fileloc)
 #end
 
 
-Dir['*.tex'].each do |file| 
+Dir["*.tex"].each do |file|
   #temp = Tempfile.new('compute_positions')
   tempname = "footempfile.txt"
   temp = File.new(tempname, "w")
@@ -64,4 +64,3 @@ Dir['*.tex'].each do |file|
   FileUtils.cp(file, "#{file}-old")
   FileUtils.cp(tempname, file)
 end
-
