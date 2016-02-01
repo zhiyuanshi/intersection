bdb320fe6e645cd98f22a19b874372a2331d14a6
João Alpuim <alpuimj@gmail.com>
2016-02-01 16:30:13 +0800

    Added CoherenceBasic.v with SExp using Locally nameless representation.

---
+++ withtop/Proofs/CoherenceBasic.v
@@ -0,0 +1,428 @@
+Require Import String.
+
+(* Notes:
+
+The syntax is encoded using Charguéraud's Locally nameless representation:
+
+http://www.chargueraud.org/softs/ln/
+
+We annotate the Coq theorems with the correnposing lemmas/theorems in the paper. 
+The reader can just look for:
+
+Lemma 5
+
+for example, to look for the proof of lemma 5 in the paper.
+
+All lemmas and theorems are complete: there are no uses of admit or Admitted. 
+
+*)
+
+(* Target language: Simply typed lambda calculus with pairs *)
+
+Inductive STyp := 
+  | STInt : STyp
+  | STFun : STyp -> STyp -> STyp
+  | STTuple : STyp -> STyp -> STyp.
+
+Inductive SExp (A : Type) :=
+  | STFVar  : A -> SExp A
+  | STBVar  : nat -> SExp A                   
+  | STLit   : nat -> SExp A
+  | STLam   : STyp -> SExp A -> SExp A
+  | STApp   : SExp A -> SExp A -> SExp A
+  | STPair  : SExp A -> SExp A -> SExp A
+  | STProj1 : SExp A -> SExp A
+  | STProj2 : SExp A -> SExp A.
+
+Definition Exp := forall A, SExp A.
+
+(* Our calculus: *)
+
+(* Types *)
+
+Inductive PTyp : Type :=
+  | PInt : PTyp
+  | Fun : PTyp -> PTyp -> PTyp
+  | And : PTyp -> PTyp -> PTyp.
+
+Fixpoint ptyp2styp (t : PTyp) : STyp :=
+  match t with
+    | PInt => STInt 
+    | Fun t1 t2 => STFun (ptyp2styp t1) (ptyp2styp t2)
+    | And t1 t2 => STTuple (ptyp2styp t1) (ptyp2styp t2)
+  end.
+
+Require Import Arith.
+Require Import Setoid.
+
+(* Subtyping relation *)
+
+Inductive Atomic : PTyp -> Prop :=
+  | AInt : Atomic PInt
+  | AFun : forall t1 t2, Atomic (Fun t1 t2).
+
+Inductive sub : PTyp -> PTyp -> Exp -> Prop :=
+  | SInt : sub PInt PInt (fun A => STLam _ STInt (STBVar _ 0))
+  | SFun : forall o1 o2 o3 o4 c1 c2, sub o3 o1 c1 -> sub o2 o4 c2 -> 
+     sub (Fun o1 o2) (Fun o3 o4) (fun A => STLam _ (ptyp2styp (Fun o1 o2)) (STLam _ (ptyp2styp o3) (STApp _ (c2 A) (STApp _ (STBVar _ 1) (STApp _ (c1 A) (STBVar _ 0))))))
+  | SAnd1 : forall t t1 t2 c1 c2, sub t t1 c1 -> sub t t2 c2 -> 
+     sub t (And  t1 t2) (fun A => STLam _ (ptyp2styp t1)
+       (STPair _ (STApp _ (c1 A) (STBVar _ 0)) (STApp _ (c2 A) (STBVar _ 0))))
+  | SAnd2 : forall t t1 t2 c, sub t1 t c -> Atomic t ->
+     sub (And  t1 t2) t (fun A => STLam _ (ptyp2styp (And t1 t2)) 
+       ((STApp _ (c A) (STProj1 _ (STBVar _ 0)))))
+  | SAnd3 : forall t t1 t2 c, sub t2 t c -> Atomic t ->
+     sub (And  t1 t2) t (fun A => STLam _ (ptyp2styp (And t1 t2)) 
+       ((STApp _ (c A) (STProj2 _ (STBVar _ 0))))).
+
+Definition Sub (t1 t2 : PTyp) : Prop := exists (e:Exp), sub t1 t2 e.
+
+(* Smart constructors for Sub *)
+
+Definition sint : Sub PInt PInt.
+unfold Sub. exists (fun A => STLam _ STInt (STBVar _ 0)). 
+exact SInt.
+Defined.
+
+Definition sfun : forall o1 o2 o3 o4, Sub o3 o1 -> Sub o2 o4 -> Sub (Fun o1 o2) (Fun  o3 o4).
+unfold Sub; intros. destruct H. destruct H0.
+exists (fun A => STLam _ (ptyp2styp (Fun o1 o2)) ( 
+       STLam _ (ptyp2styp o3) (STApp _ (x0 A) (STApp _ (STBVar _ 1) (STApp _ (x A) (STBVar _ 0)))))).
+apply SFun. auto. auto.
+Defined.
+
+Definition sand1 : forall t t1 t2, Sub t t1 -> Sub t t2 -> Sub t (And t1 t2).
+unfold Sub. intros. destruct H. destruct H0.
+exists (fun A => STLam _ (ptyp2styp t1) (
+       STPair _ (STApp _ (x A) (STBVar _ 0)) (STApp _ (x0 A) (STBVar _ 0)))).
+apply SAnd1. auto. auto.
+Defined.
+
+Definition sand2_atomic : forall t t1 t2, Sub t1 t -> Atomic t -> Sub (And  t1 t2) t.
+unfold Sub. intros. destruct t. destruct H.
+exists (fun A => STLam _ (ptyp2styp (And t1 t2)) ( 
+       (STApp _ (x A) (STProj1 _ (STBVar _ 0))))).
+apply SAnd2. auto. auto. destruct H.
+exists (fun A => STLam _ (ptyp2styp (And t1 t2)) (
+       (STApp _ (x A) (STProj1 _ (STBVar _ 0))))).
+apply SAnd2. auto. auto.
+inversion H0.
+Defined.
+
+(* The No loss of Expressivity Lemmas *)
+
+(* Theorem 3 *)
+
+Definition sand2 : forall t t1 t2, Sub t1 t -> Sub (And t1 t2) t.
+induction t; intros.
+(* Case PInt *)
+apply sand2_atomic. auto. exact AInt.
+(* Case Fun *)
+apply sand2_atomic. auto. apply AFun.
+(* Case And *)
+unfold Sub. unfold Sub in H. destruct H. inversion H.
+assert (Sub (And t0 t3) t1). apply IHt1.
+unfold Sub. exists c1. auto. 
+assert (Sub (And t0 t3) t2). apply IHt2.
+unfold Sub. exists c2. auto.
+unfold Sub in H6. destruct H6.
+unfold Sub in H7. destruct H7.
+exists (fun A => STLam _ (ptyp2styp t1) (
+       STPair _ (STApp _ (x0 A) (STBVar _ 0)) (STApp _ (x1 A) (STBVar _ 0)))).
+apply SAnd1. auto. auto.
+inversion H1.
+inversion H1.
+Defined.
+
+Definition sand3_atomic : forall t t1 t2, Sub t2 t -> Atomic t -> Sub (And t1 t2) t.
+unfold Sub. intros. destruct t. destruct H.
+exists (fun A => STLam _ (ptyp2styp (And t1 t2)) ( 
+       (STApp _ (x A) (STProj2 _ (STBVar _ 0))))).
+apply SAnd3. auto. auto. destruct H.
+exists (fun A => STLam _ (ptyp2styp (And t1 t2)) ( 
+       (STApp _ (x A) (STProj2 _ (STBVar _ 0))))).
+apply SAnd3. auto. auto.
+inversion H0.
+Defined.
+
+(* Theorem 4 *)
+
+Definition sand3 : forall t t1 t2, Sub t2 t -> Sub (And t1 t2) t.
+induction t; intros.
+(* Case PInt *)
+apply sand3_atomic. auto. exact AInt.
+(* Case Fun *)
+apply sand3_atomic. auto. apply AFun.
+(* Case And *)
+unfold Sub. unfold Sub in H. destruct H. inversion H.
+assert (Sub (And t0 t3) t1). apply IHt1.
+unfold Sub. exists c1. auto. 
+assert (Sub (And t0 t3) t2). apply IHt2.
+unfold Sub. exists c2. auto.
+unfold Sub in H6. destruct H6.
+unfold Sub in H7. destruct H7.
+exists (fun A => STLam _ (ptyp2styp t1) (
+       STPair _ (STApp _ (x0 A) (STBVar _ 0)) (STApp _ (x1 A) (STBVar _ 0)))).
+apply SAnd1. auto. auto.
+inversion H1.
+inversion H1.
+Defined.
+
+(* Disjointness: Implementation *)
+
+Inductive Ortho : PTyp -> PTyp -> Prop :=
+  | OAnd1 : forall t1 t2 t3, Ortho t1 t3 -> Ortho t2 t3 -> Ortho (And t1 t2) t3
+  | OAnd2 : forall t1 t2 t3, Ortho t1 t2 -> Ortho t1 t3 -> Ortho t1 (And t2 t3)
+  | OFun  : forall t1 t2 t3 t4, Ortho t2 t4 -> Ortho (Fun t1 t2) (Fun t3 t4)
+  | OIntFun : forall t1 t2, Ortho PInt (Fun t1 t2)
+  | OFunInt : forall t1 t2, Ortho (Fun t1 t2) PInt.
+
+(* Disjointness: Specification *)
+
+Definition OrthoS (A B : PTyp) := not (exists C, Sub A C /\ Sub B C).
+
+(* Well-formed types *)
+
+Inductive WFTyp : PTyp -> Prop := 
+  | WFInt : WFTyp PInt
+  | WFFun : forall t1 t2, WFTyp t1 -> WFTyp t2 -> WFTyp (Fun t1 t2)
+  | WFAnd : forall t1 t2, WFTyp t1 -> WFTyp t2 -> OrthoS t1 t2 -> WFTyp (And t1 t2).
+
+(* Reflexivity *)
+Hint Resolve sint sfun sand1 sand2 sand3 SInt SFun SAnd1 SAnd2 SAnd3.
+
+Lemma reflex : forall (t1 : PTyp), Sub t1 t1.
+Proof.
+induction t1; intros; auto.
+Defined.
+
+(* Disjointness algorithm is complete: Theorem 7 *)
+
+Lemma ortho_completness : forall (t1 t2 : PTyp), OrthoS t1 t2 -> Ortho t1 t2.
+Proof.
+induction t1; intros; unfold OrthoS in H.
+(* Case PInt *)
+induction t2. 
+destruct H. exists PInt. split; apply reflex.
+apply OIntFun.
+apply OAnd2. 
+apply IHt2_1. unfold not. unfold not in H. intros; apply H.
+destruct H0. destruct H0. 
+exists x. split. exact H0. apply sand2. exact H1.
+apply IHt2_2. unfold not. unfold not in H. intros. apply H.
+destruct H0. destruct H0. exists x.
+split. auto. apply sand3.
+auto.
+(* Case Fun t1 t2 *)
+induction t2.
+apply OFunInt. 
+apply OFun.
+apply IHt1_2. unfold OrthoS. unfold not. intros.
+unfold not in H. apply H.
+destruct H0. destruct H0.
+exists (Fun (And t1_1 t2_1) x).
+split.
+apply sfun.
+apply sand2.
+apply reflex.
+auto.
+apply sfun.
+apply sand3. apply reflex.
+auto.
+(* Case t11 -> t12 _|_ t21 & t22 *)
+apply OAnd2.
+apply IHt2_1.
+unfold not. unfold not in H. intros. apply H.
+destruct H0. destruct H0.
+exists x. split. auto. apply sand2. exact H1.
+apply IHt2_2.
+unfold not. unfold not in H. intros. apply H.
+destruct H0. destruct H0.
+exists x. split. auto. apply sand3. exact H1.
+(* Case (t11 & t12) _|_ t2 *) 
+apply OAnd1.
+apply IHt1_1.
+unfold OrthoS.
+unfold not. unfold not in H.
+intro.
+apply H.
+clear H. destruct H0. destruct H.
+exists x.
+split.
+apply sand2. exact H.
+exact H0.
+apply IHt1_2.
+unfold OrthoS; unfold not; intro. unfold not in H.
+apply H. clear H.
+destruct H0.
+destruct H.
+exists x.
+split.
+apply sand3.
+exact H.
+exact H0.
+Defined.
+
+Lemma nosub : forall t1 t2, OrthoS t1 t2 -> not (Sub t1 t2) /\ not (Sub t2 t1).
+Proof.
+intros; split; unfold not.
+unfold OrthoS in H. unfold not in H. intros.
+apply H.
+exists t2.
+split. auto. apply reflex.
+unfold OrthoS in H. unfold not in H. intros.
+apply H.
+exists t1. split. apply reflex. auto.
+Defined.
+
+
+Lemma invAndS1 : forall t t1 t2, Sub t (And t1 t2) -> Sub t t1 /\ Sub t t2.
+Proof.
+induction t; intros.
+(* Case Int *)
+inversion H. inversion H0. split; unfold Sub. exists c1. auto. exists c2. auto.
+(* Case Fun *)
+inversion H. inversion H0. split; unfold Sub. exists c1. auto. exists c2. auto.
+(* Case And *)
+inversion H. inversion H0. split; unfold Sub. exists c1. auto. exists c2. auto.
+assert (Sub t1 t0 /\ Sub t1 t3).
+apply IHt1.
+auto. unfold Sub. exists c. auto.
+destruct H7.
+split.
+apply sand2.
+auto.
+apply sand2.
+auto.
+assert (Sub t2 t0 /\ Sub t2 t3).
+apply IHt2.
+unfold Sub. exists c. auto.
+destruct H7.
+split.
+apply sand3.
+auto.
+apply sand3.
+auto.
+Defined.
+
+(* Unique subtype contributor: Lemma 4 *)
+
+Lemma uniquesub : forall A B C, 
+  OrthoS A B -> Sub (And A B) C -> not (Sub A C /\ Sub B C).
+Proof.
+intros. unfold OrthoS in H. unfold not. intros. apply H. exists C. auto.
+Defined.
+
+(* Lemmas needed to prove soundness of the disjointness algorithm *)
+
+Lemma ortho_sym : forall A B, OrthoS A B -> OrthoS B A.
+Proof.
+unfold OrthoS. unfold not.
+intros. apply H.
+destruct H0. destruct H0.
+exists x.
+split; auto.
+Defined.
+
+Lemma ortho_and : forall A B C, OrthoS A C -> OrthoS B C -> OrthoS (And A B) C.
+Proof.
+intros. unfold OrthoS.
+unfold not. intros.
+destruct H1. destruct H1.
+induction x. 
+inversion H1. inversion H3. unfold OrthoS in H. apply H. exists (PInt). split. 
+unfold Sub. exists c. auto. unfold Sub.  unfold Sub in H2. destruct H2. exists x0. auto.
+unfold OrthoS in H0. apply H0. exists (PInt). split. 
+unfold Sub. exists c. auto. unfold Sub.  unfold Sub in H2. destruct H2. exists x0. auto.
+inversion H1. inversion H3. unfold OrthoS in H. apply H. exists (Fun x1 x2). split. 
+unfold Sub. exists c. auto. unfold Sub.  unfold Sub in H2. destruct H2. exists x0. auto.
+unfold OrthoS in H0. apply H0. exists (Fun x1 x2). split. 
+unfold Sub. exists c. auto. unfold Sub.  unfold Sub in H2. destruct H2. exists x0. auto.
+assert (Sub C x1 /\ Sub C x2). apply invAndS1. auto. destruct H3.
+inversion H1. inversion H5. apply IHx1. 
+unfold Sub. exists c1. auto. unfold Sub.  unfold Sub in H3. destruct H3. exists x0. auto.
+unfold OrthoS in H. apply H. exists (And x1 x2). split. 
+unfold Sub. exists c. auto. unfold Sub.  unfold Sub in H2. destruct H2. exists x0. auto.
+unfold OrthoS in H0. apply H0. exists (And x1 x2). split. 
+unfold Sub. exists c. auto. unfold Sub.  unfold Sub in H2. destruct H2. exists x0. auto.
+Defined.
+
+(* Soundness of the disjointness algorithm: Theorem 6 *)
+
+Lemma ortho_soundness : forall (t1 t2 : PTyp), Ortho t1 t2 -> OrthoS t1 t2.
+intros.
+induction H.
+(* Hard case *)
+apply ortho_and; auto.
+assert (OrthoS t2 t1). apply ortho_sym. apply IHOrtho1; auto.
+assert (OrthoS t3 t1). apply ortho_sym. apply IHOrtho2; auto.
+apply ortho_sym.
+apply ortho_and; auto.
+(* Case FunFun *)
+unfold OrthoS. unfold not. intros.
+unfold OrthoS in IHOrtho. apply IHOrtho.
+destruct H0. destruct H0. generalize H0. generalize H1. clear H0. clear H1.
+induction x; intros. inversion H1. inversion H2. exists x2.
+split. inversion H0. inversion H2. unfold Sub. exists c2. auto. unfold Sub. inversion H1. inversion H2. exists c2. auto.
+apply IHx1.
+inversion H1. inversion H2. unfold Sub. exists c1. auto. 
+inversion H0. inversion H2. exists c1. auto.
+(* Case IntFun *)
+unfold OrthoS. unfold not. intros.
+destruct H. destruct H. induction x. inversion H0. inversion H1. inversion H. inversion H1.
+apply IHx1.
+inversion H. inversion H1. unfold Sub. exists c1. auto.
+inversion H0. inversion H1. unfold Sub. exists c1. auto.
+(* Case FunInt *)
+unfold OrthoS. unfold not. intros.
+destruct H. destruct H. induction x. inversion H. inversion H1. inversion H0. inversion H1.
+apply IHx1. inversion H. inversion H1. unfold Sub. exists c1. auto.
+inversion H0. inversion H1. unfold Sub. exists c1. auto.
+Defined.
+
+(* Coercive subtyping is coeherent: Lemma 5 *)
+
+Lemma sub_coherent : forall A, WFTyp A -> forall B, WFTyp B -> forall C1, sub A B C1 -> forall C2, sub A B C2 -> C1 = C2.
+Proof.
+intro. intro. intro. intro. intro. intro.
+(* Case: Int <: Int *)
+induction H1; intros.
+inversion H1. 
+reflexivity.
+(* Case: Fun t1 t2 <: Fun t3 t4 *)
+inversion H1; inversion H; inversion H0.
+assert (c2 = c3). apply IHsub2; auto.
+assert (c1 = c0). apply IHsub1; auto.
+rewrite H17. rewrite H18.
+reflexivity.
+(* Case: t <: And t1 t2 *) 
+inversion H1; inversion H0.
+assert (c1 = c0). apply IHsub1; auto.
+assert (c2 = c3). apply IHsub2; auto.
+rewrite H13. rewrite H14.
+reflexivity.
+(* different coercion case*)
+inversion H3.
+(* different coercion case*)
+inversion H3.
+(* Case: And t1 t2 <: t (first) *)
+inversion H3; inversion H.
+(* different coercion *)
+rewrite <- H7 in H2. inversion H2.
+(* same coercion *)
+assert (c = c0). apply IHsub; auto. rewrite H15.
+reflexivity.
+(* contradiction: not orthogonal! *)
+destruct H14. exists t. unfold Sub.
+split. exists c; auto. exists c0. auto.
+(* Case: And t1 t2 <: t (second) *)
+inversion H3; inversion H.
+rewrite <- H7 in H2. inversion H2.
+(* contradiction: not orthogonal! *)
+destruct H14. exists t. unfold Sub.
+split. exists c0; auto. exists c. auto.
+(* same coercion; no contradiction *)
+assert (c = c0). apply IHsub; auto. rewrite H15.
+reflexivity.
+Defined.
+
